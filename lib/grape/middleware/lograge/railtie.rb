53be90fb68ca8c0db2be83c71cea4d1c7a1ca888
Sami Samhuri <sami@samhuri.net>
2021-01-11 16:16:36 -0800

    Use ActiveSupport::ParamaterFilter for Rails 6 compatibility

--- lib/grape/middleware/lograge/railtie.rb
+++ lib/grape/middleware/lograge/railtie.rb
@@ -1,6 +1,6 @@
 class Grape::Middleware::Lograge::Railtie < Rails::Railtie
   initializer 'grape.middleware.lograge', after: :load_config_initializers do
-    Grape::Middleware::Lograge.filter = ActionDispatch::Http::ParameterFilter.new Rails.application.config.filter_parameters
+    Grape::Middleware::Lograge.filter = ActiveSupport::ParameterFilter.new Rails.application.config.filter_parameters
 
     ::Lograge::LogSubscribers::ActionController.attach_to :grape
   end
