e44f3941b1feb941f4a1ce4b57f60795e746e2a3
Sami Samhuri <sami@samhuri.net>
2021-02-10 20:48:14 -0800

    Release 1.3.1 with Rails 6 support

--- lib/grape/middleware/lograge/railtie.rb
+++ lib/grape/middleware/lograge/railtie.rb
@@ -1,6 +1,12 @@
 class Grape::Middleware::Lograge::Railtie < Rails::Railtie
   initializer 'grape.middleware.lograge', after: :load_config_initializers do
-    Grape::Middleware::Lograge.filter = ActiveSupport::ParameterFilter.new Rails.application.config.filter_parameters
+    if defined?(ActiveSupport::ParameterFilter)
+      # Rails 6
+      Grape::Middleware::Lograge.filter = ActiveSupport::ParameterFilter.new Rails.application.config.filter_parameters
+    else
+      # Rails 5
+      Grape::Middleware::Lograge.filter = ActionDispatch::Http::ParameterFilter.new Rails.application.config.filter_parameters
+    end
 
     ::Lograge::LogSubscribers::ActionController.attach_to :grape
   end
